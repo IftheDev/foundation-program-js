493b4a7432a02073bfc07c7027e09804a2c2f62e
Ifthe16 <habiburrahmanif975@gmail.com>
2026-08-25 07:38:28 +0600

    added variable.js file

---
+++ variable.js
@@ -0,0 +1,13 @@
+let name = "Rahim";
+name = "Karim";
+console.log(name);
+
+const age = 21;
+// age = 20;
+console.log(age);
+
+console.log(typeof name, typeof age);
+// console.log(typeof (name), typeof (age));
+
+const isAdult = true;
+console.log(typeof isAdult);
